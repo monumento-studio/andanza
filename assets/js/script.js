c76009cff21787103e080f21f798c66bb0a01bab
Luis Benitez <luisbttf@gmail.com>
2018-04-05 18:48:37 -0500

    Andanza web V1 - Front-End
    Andanza web V1 - Front-End

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(".ham").on('click', function(){
+  $(this).toggleClass('clicked');
+  $("nav.resp").toggleClass('navhe');
+});
+
+$(".tienda").on("click", function(){
+  $(this).toggleClass('menos');
+});
+ 
